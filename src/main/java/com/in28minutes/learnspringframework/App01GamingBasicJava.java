1bab20f03432d18fafefcda5272f5b2af8aa578f
Jimmy Tran <jimmytrandev@gmail.com>
2023-12-19 17:26:45 +0100

    feat: ✨ do 16

--- src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/in28minutes/learnspringframework/App01GamingBasicJava.java
@@ -7,10 +7,14 @@ import com.in28minutes.learnspringframework.game.SuperContraGame;
 
 public class App01GamingBasicJava {
   public static void main(String[] args) {
+
     // var game = new MarioGame();
     // var game = new SuperContraGame();
     var game = new PacmanGame();
+
     var gameRunner = new GameRunner(game);
+
     gameRunner.run();
+
   }
 }
