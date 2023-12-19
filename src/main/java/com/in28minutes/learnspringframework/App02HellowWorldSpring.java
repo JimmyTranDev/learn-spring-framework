1848f8f2240d5a2a6738e97487071bd430284d45
Jimmy Tran <jimmytrandev@gmail.com>
2023-12-19 14:34:27 +0100

    feat: ✨ add person and address beans

--- src/main/java/com/in28minutes/learnspringframework/App02HellowWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HellowWorldSpring.java
@@ -6,10 +6,15 @@ public class App02HellowWorldSpring {
 
   public static void main(String[] args) {
     // 1: Launch a Spring Context
-    // 2: Configure the things that we want Spring to manage
     var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
+    // 2: Configure the things that we want Spring to manage
+
+    // 3: Get the beans managed by Spring
     System.out.println(context.getBean("name"));
+    System.out.println(context.getBean("age"));
+    System.out.println(context.getBean("person"));
+    System.out.println(context.getBean("address"));
   }
 
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,12 +3,35 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {
+};
+
+record Address(String firstLine, String city) {
+};
+
 @Configuration
 class HelloWorldConfiguration {
 
   @Bean
   public String name() {
     return "Ranga";
+  }
+
+  @Bean
+  public int age() {
+    return 15;
+  }
+
+  @Bean
+  public Person person() {
+    var person = new Person("Ravi", 20);
+    return person;
+  }
+
+  @Bean
+  public Address address() {
+    var address = new Address("Baker Street", "London");
+    return address;
   }
 
 }
