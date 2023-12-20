a46270c81042f5e04c137155fc43ffb500996806
Jimmy Tran <jimmytrandev@gmail.com>
2023-12-20 15:34:13 +0100

    feat: ✨ 4.13

--- src/main/java/com/in28minutes/learnspringframework/examples/c1/MySQLDataService.java
+++ src/main/java/com/in28minutes/learnspringframework/examples/c1/MySQLDataService.java
@@ -1,8 +1,9 @@
 package com.in28minutes.learnspringframework.examples.c1;
 
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+// @Component
+@Repository
 class MySQLDataService implements DataService {
   public int[] retrieveAllData() {
     return new int[] { 1, 2, 3, 4, 5 };
--- src/main/java/com/in28minutes/learnspringframework/examples/g1/SimpleSpringContextLauncherApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/examples/g1/SimpleSpringContextLauncherApplication.java
@@ -2,17 +2,17 @@ package com.in28minutes.learnspringframework.examples.g1;
 
 import java.util.Arrays;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 
 // @Component
-@Named
+// @Named
+@Service
 class BusinessService {
   private DataService dataService;
 
